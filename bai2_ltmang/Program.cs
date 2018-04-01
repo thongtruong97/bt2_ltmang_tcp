9bbb8d866391a3be5633c889908678535dcf74b1
thong <tqt15111997@gmail.com>
2018-04-01 18:44:04 +0700

    tcp

---
+++ bai2_ltmang/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+namespace bai2_ltmang
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            IPEndPoint ipserver = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 80);
+            Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            client.SendTimeout = 1000;
+            client.ReceiveTimeout = 1000;
+            //TcpClient c = new TcpClient();
+            //c.ReceiveTimeout = 0;
+            try
+            {
+                client.Connect(ipserver);
+                Console.WriteLine(ipserver.ToString() + " is opened.");
+                client.Close();
+            }
+            catch
+            {
+                Console.WriteLine(ipserver.ToString() + " is closed.");
+            }
+        }
+    }
+}
